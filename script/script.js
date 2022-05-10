2be5bf4bfc5dc93bced30a58968cff958da94f7a
Javier Perez <javierpdc13@hotmail.com>
2022-05-10 13:14:56 -0400

    Add event listeners for hovering over squares and clicking button

--- script/script.js
+++ script/script.js
@@ -1,12 +1,45 @@
 const container = document.querySelector(".container");
 
+//draws an amount of squares depending on the dimensions specified. Default is 16x16
 function drawSquares(dimensions = 16) {
     for (i = 0; i < (dimensions * dimensions); i++) {
         let square = document.createElement("div");
         square.classList.add("square");
-        square.textContent = "h";
         container.appendChild(square);
+    }
+
+    container.style.cssText += `grid-template-columns: repeat(${dimensions}, 1fr); 
+    grid-auto-rows: minmax(20px, auto)`;
+
+    const squares = document.querySelectorAll(".square");
+
+    for (let square of squares) {
+        square.addEventListener("mouseover", () => {
+            square.classList.add("Active");
+        });
     }
 }
 
 drawSquares();
+
+const button = document.querySelector("button");
+
+
+button.addEventListener("click", () => {
+    let dimensions = prompt("How big you want your canvas to be?");
+    if (dimensions > 64) {
+        alert("The canvas can't be bigger than 64x64");
+    }
+    else {
+        
+        const squares = document.querySelectorAll(".square");
+
+        for (let square of squares) {
+            square.remove();
+        }
+
+        drawSquares(dimensions)
+    }
+});
+
+
